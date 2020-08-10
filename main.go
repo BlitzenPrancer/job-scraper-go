c6a2bd0e6c3b150ac9d1603048ebd303a8c5ab24
Mina Shin <me@minashin.net>
2020-08-09 20:12:09 -0700

    Change code to use goroutines for concurrency

--- main.go
+++ main.go
@@ -22,39 +22,51 @@ var baseURL string = "https://www.indeed.com/jobs?limit=50&l=Seattle&q=software+
 
 func main() {
 	var jobs []jobItem
+	mainC := make(chan []jobItem)
 	pages := getNumOfPages()
 
 	for i := 0; i < pages; i++ {
-		jobItemsOnPage := getPage(i)
+		go getPage(i, mainC)
+	}
+
+	for i := 0; i < pages; i++ {
+		jobItemsOnPage := <-mainC
 		jobs = append(jobs, jobItemsOnPage...)
 	}
 
-	fmt.Println(jobs[0])
-
+	fmt.Println("Done")
 }
 
 // getPage parses the page and returns jobs on that page
-func getPage(page int) []jobItem {
+func getPage(page int, mainC chan<- []jobItem) {
 	var jobsOnPage []jobItem
-	pageURL := baseURL + "&start=" + strconv.Itoa(page*50)
+	c := make(chan jobItem)
 
+	pageURL := baseURL + "&start=" + strconv.Itoa(page*50)
 	res, err := http.Get(pageURL)
+	fmt.Println("Requesting", pageURL)
+
 	CheckError(err)
 	defer res.Body.Close()
 	CheckCode(res)
 
 	doc, err := goquery.NewDocumentFromReader(res.Body)
 	CheckError(err)
+
 	jobCards := doc.Find(".jobsearch-SerpJobCard")
 	jobCards.Each(func(i int, card *goquery.Selection) {
-		job := initJobItem(card)
-		jobsOnPage = append(jobsOnPage, job)
+		go initJobItem(card, c)
 	})
 
-	return jobsOnPage
+	for i := 0; i < jobCards.Length(); i++ {
+		job := <-c
+		jobsOnPage = append(jobsOnPage, job)
+	}
+
+	mainC <- jobsOnPage
 }
 
-func initJobItem(card *goquery.Selection) jobItem {
+func initJobItem(card *goquery.Selection, c chan<- jobItem) {
 	id, _ := card.Attr("data-jk")
 	title := CleanString(card.Find(".title > a").Text())
 	company := CleanString(card.Find(".company > a").Text())
@@ -62,7 +74,7 @@ func initJobItem(card *goquery.Selection) jobItem {
 	salary := CleanString(card.Find(".salaryText").Text())
 	summary := CleanString(card.Find(".summary").Text())
 
-	return jobItem{id: id, title: title, company: company, location: location, salary: salary, summary: summary}
+	c <- jobItem{id: id, title: title, company: company, location: location, salary: salary, summary: summary}
 }
 
 // getNumOfPages gets total number of pages from the result.
